96a03415b27c954e43eba700998a5cfb981b9426
Name707 <707VLAN@gmail.com>
2023-01-29 19:13:44 +0600

    KS-11-3 Задача - описание сущностей для упрощенной версии социальной сети

---
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -0,0 +1,137 @@
+package lesson_11
+
+/*
+
+Задача 3 к Уроку 11
+
+Нужно описать сущности для упрощенной версии приложения социальной сети, в которой общаются только голосом.
+
+Функционал одного из модулей будет такой.
+Есть лента, в которой отображаются “комнаты” для общения по интересам. В карточке “комнаты” отображается:
+ – обложка
+ – название
+ – список участников, которые отображаются в виде аватарок. При долгом нажатии на аватар подсветится его никнейм.
+ Рядом с аватаркой отображается текстовый бейдж с одним из возможных статусов говорящего:
+ “разговаривает”, “микрофон выключен”, “пользователь заглушен”.
+
+После проектирования создай список объектов карточек с несколькими “комнатами” с произвольными данными.
+
+
+ */
+
+fun main() {
+
+    val rockUsers = listOf(User("Аватар-Гитара.jpg", "Рок-участник №1"),
+        User("Аватар-Барабаны.jpg", "Рок-участник №2"))
+
+    val classicalUsers = listOf(User("Аватар-Скрипка.jpg", "Классик-участник №1"),
+        User("Аватар-Фортепиано.jpg", "Классик-участник №2"))
+
+    val rockMusicRoom = Room("Рок-обложка комнаты",
+        "Название комнаты Рокеры",
+        rockUsers)
+
+    val classicalMusicRoom = Room("Классическая обложка комнаты",
+        "Название комнаты Классика",
+        classicalUsers)
+
+}
+
+class Room(
+    cover: String,
+    name: String,
+    users: List<User>,
+)
+
+class User(
+    avatarParameter: String,
+    nameParameter: String,
+) {
+
+    private val name = nameParameter
+    private var userStatus = "Микрофон выключен"
+    private var longTapActivated = "Отображение имени отключено"
+
+    fun avatarLongTap(tap: Boolean = false): String {
+        if (tap) longTapActivated = "Отображение имени включено"
+        return longTapActivated
+    }
+
+    fun userVoiceStatus(getStatus: String): String {
+
+        when(getStatus) {
+            "Включить микрофон" -> userStatus = "разговаривает"
+            "Выключить микрофон" -> userStatus = "микрофон выключен"
+            "Заглушить" -> userStatus = "пользователь заглушен"
+        }
+        return userStatus
+
+
+    }
+
+}
+
+/*
+
+fun main() {
+
+    val RockUserOne = UserRockRoom()
+
+}
+
+abstract class Room {
+
+    abstract val roomCover: String
+    abstract val roomName: String
+    abstract val usersList: List<String>
+    fun voiceStatus(status: String): String {
+        when(status) {
+            "Включить микрофон" -> return "разговаривает"
+            "Отключить микрофон" -> return "микрофон выключен"
+            "Заглушить" -> return "пользователь заглушен"
+        }
+        return status
+    }
+}
+
+abstract class RockMusicRoom: Room() {
+
+    override val roomCover = "Рок-обложка.jpg"
+    override val roomName = "Группа рок-музыки"
+
+}
+
+abstract class ClassicalMusicRoom: Room() {
+
+    override val roomCover = "Пианино.jpg"
+    override val roomName = "Классическая музыка"
+
+}
+
+class UserRockRoom(name: String, ): RockMusicRoom() {
+
+    val userAvatar = ""
+    private val userName = ""
+
+    fun userIconLongTap(tap: Boolean): String {
+
+        if(tap) return userName
+        return userName
+
+    }
+ }
+
+abstract class UserClassicalRoom: ClassicalMusicRoom() {
+
+    val userAvatar = ""
+    private val userName = ""
+
+    fun userIconLongTap(tap: Boolean): String {
+
+        if(tap) return userName
+        return userName
+
+    }
+}
+*/
+
