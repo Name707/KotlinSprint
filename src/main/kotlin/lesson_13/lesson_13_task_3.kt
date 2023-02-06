12a65070983824a6cc4e3ba8b79a2dfa904ee94d
Name707 <707VLAN@gmail.com>
2023-02-06 20:36:59 +0600

    KS-13-3 Создание пустого списка-справочника и добавление в него новых объектов

---
+++ src/main/kotlin/lesson_13/lesson_13_task_3.kt
@@ -0,0 +1,41 @@
+package lesson_13
+
+/*
+
+Задача 3 к Уроку 13
+
+Для того чтобы взаимодействовать со всеми записями телефонной книги, как с одним целым, их нужно объединить в список.
+
+Создай пустой список, добавь в него несколько объектов, инициализируя их с разнообразными данными (в том числе и null).
+
+Напиши функцию, которая будет выводить имя, номер телефона и компанию.
+Если какого-либо значения нет – программа должна заменить его на строку [не указано].
+С помощью этой функции выведи в консоль все объекты из списка.
+
+ */
+
+fun main() {
+
+    val phoneBook: MutableList<UserContacts> = mutableListOf()
+    phoneBook.add(UserContacts("Ростислав", 89123456789, "Reddit"))
+    phoneBook.add(UserContacts("Вячеслав", null, "GitHub"))
+    phoneBook.add(UserContacts("Анна", 81561211651, null))
+
+    for (item in phoneBook) item.userInfoPrinter()
+
+}
+
+class UserContacts(
+    val name: String?,
+    val phoneNumber: Long?,
+    val company: String?,
+) {
+
+    fun userInfoPrinter() {
+        val nameNullChecker = name ?: "[не указано]"
+        val namePhoneNumber = phoneNumber ?: "[не указано]"
+        val nameCompanyChecker = company ?: "[не указано]"
+
+        println("Имя: $nameNullChecker \nНомер: $namePhoneNumber \nКомпания: $nameCompanyChecker\n")
+    }
+}
