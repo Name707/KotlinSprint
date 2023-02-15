8e600e834ab8851eb11a29449d2796270ccc8653
Name707 <707VLAN@gmail.com>
2023-02-15 19:12:39 +0600

    KS-15-1 Создание интерфейсов для животных с разным способом передвижения.

---
+++ src/main/kotlin/lesson_15/lesson_15_task_1.kt
@@ -0,0 +1,63 @@
+package lesson_15
+
+/*
+
+Задача 1 к Уроку 15
+
+В мобильной игре есть разные существа. Напиши интерфейсы для летающих и плавающих существ.
+
+ – создай интерфейсы с методами передвижения для следующих существ: карась, чайка, утка;
+ – затем создай для каждого существа классы, реализующие соответствующие интерфейсы;
+ – выведи сообщения в консоль, о том как они могут передвигаться.
+
+ */
+
+fun main() {
+
+    val crucianCarp = CrucianCarp()
+    crucianCarp.swimming()
+
+    val gull = Gull()
+    gull.flying()
+
+    val duck = Duck()
+    duck.flying()
+
+
+}
+
+interface Swim {
+
+    fun swimming() {
+        println("It is a swimming animal")
+    }
+
+}
+
+interface Fly {
+
+    fun flying() {
+        println("It is a flying animal")
+    }
+}
+
+class CrucianCarp() : Swim {
+
+    override fun swimming() {
+        println("Crucian Carp swimming animal")
+    }
+}
+
+class Gull() : Fly {
+
+    override fun flying() {
+        println("Gull flying animal")
+    }
+}
+
+class Duck() : Fly {
+
+    override fun flying() {
+        println("Duck flying animal")
+    }
+}
