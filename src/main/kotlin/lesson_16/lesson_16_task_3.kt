384643e3fcd453e4eaf3a490dbcfcce1fefb66ef
n707 <707vlad@gmail.com>
2023-02-18 22:55:51 +0600

    KS-16-3 Создание клсса User, содержащий лоигн и пароль пользователя. Пароль долен быть проверен на правильность при условии, что не должен быть виден в телеглавной функции

---
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -0,0 +1,31 @@
+package lesson_16
+
+/*
+
+Задача 3 к Уроку 16
+
+Создай класс, описывающий пользователя. У него должен быть логин и пароль.
+Пароль не должен быть виден в теле главной функции, но должен быть способ проверить, что пароль введен верно.
+Выведи информацию о релевантности пароля в консоль.
+
+ */
+
+fun main() {
+
+    val user = User("Name707", "pass808")
+    user.comparisonPasswords()
+}
+
+class User(
+    val userLoginInputed: String,
+    val userPassInputed: String,
+) {
+
+    private val userPassword = "pass707"
+
+    fun comparisonPasswords() {
+        if (userPassInputed == userPassword) println("Введенный пароль верный")
+        else println("Введенный пароль неверный")
+    }
+
+}
